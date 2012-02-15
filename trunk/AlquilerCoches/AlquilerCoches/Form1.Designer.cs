3af826a2884146a0e45192621965dbbfea2f39e8
javiumbe <javiumbe@99eea89f-4a7c-9688-262d-877626069f20>
2012-02-15 16:52:37 +0000

    Añadido label y textbox para probar si funciona bien el SVN

--- trunk/AlquilerCoches/AlquilerCoches/Form1.Designer.cs
+++ trunk/AlquilerCoches/AlquilerCoches/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.TbotonOK = new System.Windows.Forms.Button();
+            this.TlabelMarca = new System.Windows.Forms.Label();
+            this.TtextBoxMarca = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // TbotonOK
@@ -40,21 +42,42 @@
             this.TbotonOK.Text = "button1";
             this.TbotonOK.UseVisualStyleBackColor = true;
             // 
+            // TlabelMarca
+            // 
+            this.TlabelMarca.AutoSize = true;
+            this.TlabelMarca.Location = new System.Drawing.Point(37, 41);
+            this.TlabelMarca.Name = "TlabelMarca";
+            this.TlabelMarca.Size = new System.Drawing.Size(37, 13);
+            this.TlabelMarca.TabIndex = 1;
+            this.TlabelMarca.Text = "Marca";
+            // 
+            // TtextBoxMarca
+            // 
+            this.TtextBoxMarca.Location = new System.Drawing.Point(122, 41);
+            this.TtextBoxMarca.Name = "TtextBoxMarca";
+            this.TtextBoxMarca.Size = new System.Drawing.Size(100, 20);
+            this.TtextBoxMarca.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.TtextBoxMarca);
+            this.Controls.Add(this.TlabelMarca);
             this.Controls.Add(this.TbotonOK);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button TbotonOK;
+        private System.Windows.Forms.Label TlabelMarca;
+        private System.Windows.Forms.TextBox TtextBoxMarca;
     }
 }
 
