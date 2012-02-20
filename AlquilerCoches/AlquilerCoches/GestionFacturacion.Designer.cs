7997225798946851d1f6c9e8815610719d629a0c
adrims1986 <adrims1986@gmail.com>
2012-02-20 13:38:56 +0000

    Segundo form facturacion

---
+++ AlquilerCoches/AlquilerCoches/GestionFacturacion.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace AlquilerCoches
+{
+    partial class GestionFacturacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TLabelNFactura = new System.Windows.Forms.Label();
+            this.TTextBoxNFactura = new System.Windows.Forms.TextBox();
+            this.TButtonBuscar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TLabelUsuario = new System.Windows.Forms.Label();
+            this.TLabelEmpresa = new System.Windows.Forms.Label();
+            this.TLabelCategoria = new System.Windows.Forms.Label();
+            this.TLabelVehiculo = new System.Windows.Forms.Label();
+            this.TLabelConductores = new System.Windows.Forms.Label();
+            this.TLabelTarifa = new System.Windows.Forms.Label();
+            this.TLabelTiempo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TLabelPSinIva = new System.Windows.Forms.Label();
+            this.TLabelTIva = new System.Windows.Forms.Label();
+            this.TLabelPTotal = new System.Windows.Forms.Label();
+            this.TTextBoxCategoria = new System.Windows.Forms.TextBox();
+            this.TTextBoxVehiculo = new System.Windows.Forms.TextBox();
+            this.TTextBoxConductores = new System.Windows.Forms.TextBox();
+            this.TTextBoxTarifa = new System.Windows.Forms.TextBox();
+            this.TTextBoxTiempo = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TTextBoxTPSinIva = new System.Windows.Forms.TextBox();
+            this.TTextBoxTIva = new System.Windows.Forms.TextBox();
+            this.TTextBoxPTotal = new System.Windows.Forms.TextBox();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.rectangleShape1 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.TButtonDuplicar = new System.Windows.Forms.Button();
+            this.TButtonModificar = new System.Windows.Forms.Button();
+            this.TButtonEliminar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TLabelNFactura
+            // 
+            this.TLabelNFactura.AutoSize = true;
+            this.TLabelNFactura.Location = new System.Drawing.Point(21, 22);
+            this.TLabelNFactura.Name = "TLabelNFactura";
+            this.TLabelNFactura.Size = new System.Drawing.Size(61, 13);
+            this.TLabelNFactura.TabIndex = 0;
+            this.TLabelNFactura.Text = "Nº Factura:";
+            // 
+            // TTextBoxNFactura
+            // 
+            this.TTextBoxNFactura.Location = new System.Drawing.Point(88, 19);
+            this.TTextBoxNFactura.Name = "TTextBoxNFactura";
+            this.TTextBoxNFactura.Size = new System.Drawing.Size(56, 20);
+            this.TTextBoxNFactura.TabIndex = 1;
+            // 
+            // TButtonBuscar
+            // 
+            this.TButtonBuscar.Location = new System.Drawing.Point(168, 15);
+            this.TButtonBuscar.Name = "TButtonBuscar";
+            this.TButtonBuscar.Size = new System.Drawing.Size(58, 27);
+            this.TButtonBuscar.TabIndex = 2;
+            this.TButtonBuscar.Text = "Buscar";
+            this.TButtonBuscar.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 20);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Buscar por cliente";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // TLabelUsuario
+            // 
+            this.TLabelUsuario.AutoSize = true;
+            this.TLabelUsuario.Location = new System.Drawing.Point(28, 104);
+            this.TLabelUsuario.Name = "TLabelUsuario";
+            this.TLabelUsuario.Size = new System.Drawing.Size(83, 13);
+            this.TLabelUsuario.TabIndex = 21;
+            this.TLabelUsuario.Text = "Nombre Usuario";
+            // 
+            // TLabelEmpresa
+            // 
+            this.TLabelEmpresa.AutoSize = true;
+            this.TLabelEmpresa.Location = new System.Drawing.Point(272, 104);
+            this.TLabelEmpresa.Name = "TLabelEmpresa";
+            this.TLabelEmpresa.Size = new System.Drawing.Size(78, 13);
+            this.TLabelEmpresa.TabIndex = 22;
+            this.TLabelEmpresa.Text = "Datos empresa";
+            // 
+            // TLabelCategoria
+            // 
+            this.TLabelCategoria.AutoSize = true;
+            this.TLabelCategoria.Location = new System.Drawing.Point(50, 146);
+            this.TLabelCategoria.Name = "TLabelCategoria";
+            this.TLabelCategoria.Size = new System.Drawing.Size(57, 13);
+            this.TLabelCategoria.TabIndex = 23;
+            this.TLabelCategoria.Text = "Categoría:";
+            // 
+            // TLabelVehiculo
+            // 
+            this.TLabelVehiculo.AutoSize = true;
+            this.TLabelVehiculo.Location = new System.Drawing.Point(54, 178);
+            this.TLabelVehiculo.Name = "TLabelVehiculo";
+            this.TLabelVehiculo.Size = new System.Drawing.Size(53, 13);
+            this.TLabelVehiculo.TabIndex = 24;
+            this.TLabelVehiculo.Text = "Vehículo:";
+            // 
+            // TLabelConductores
+            // 
+            this.TLabelConductores.AutoSize = true;
+            this.TLabelConductores.Location = new System.Drawing.Point(37, 208);
+            this.TLabelConductores.Name = "TLabelConductores";
+            this.TLabelConductores.Size = new System.Drawing.Size(70, 13);
+            this.TLabelConductores.TabIndex = 25;
+            this.TLabelConductores.Text = "Conductores:";
+            // 
+            // TLabelTarifa
+            // 
+            this.TLabelTarifa.AutoSize = true;
+            this.TLabelTarifa.Location = new System.Drawing.Point(70, 240);
+            this.TLabelTarifa.Name = "TLabelTarifa";
+            this.TLabelTarifa.Size = new System.Drawing.Size(37, 13);
+            this.TLabelTarifa.TabIndex = 26;
+            this.TLabelTarifa.Text = "Tarifa:";
+            // 
+            // TLabelTiempo
+            // 
+            this.TLabelTiempo.AutoSize = true;
+            this.TLabelTiempo.Location = new System.Drawing.Point(32, 270);
+            this.TLabelTiempo.Name = "TLabelTiempo";
+            this.TLabelTiempo.Size = new System.Drawing.Size(75, 13);
+            this.TLabelTiempo.TabIndex = 27;
+            this.TLabelTiempo.Text = "Tiempo (días):";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(217, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Nº Factura";
+            // 
+            // TLabelPSinIva
+            // 
+            this.TLabelPSinIva.AutoSize = true;
+            this.TLabelPSinIva.Location = new System.Drawing.Point(36, 339);
+            this.TLabelPSinIva.Name = "TLabelPSinIva";
+            this.TLabelPSinIva.Size = new System.Drawing.Size(75, 13);
+            this.TLabelPSinIva.TabIndex = 29;
+            this.TLabelPSinIva.Text = "Precio Sin IVA";
+            // 
+            // TLabelTIva
+            // 
+            this.TLabelTIva.AutoSize = true;
+            this.TLabelTIva.Location = new System.Drawing.Point(164, 339);
+            this.TLabelTIva.Name = "TLabelTIva";
+            this.TLabelTIva.Size = new System.Drawing.Size(51, 13);
+            this.TLabelTIva.TabIndex = 30;
+            this.TLabelTIva.Text = "Total IVA";
+            // 
+            // TLabelPTotal
+            // 
+            this.TLabelPTotal.AutoSize = true;
+            this.TLabelPTotal.Location = new System.Drawing.Point(286, 339);
+            this.TLabelPTotal.Name = "TLabelPTotal";
+            this.TLabelPTotal.Size = new System.Drawing.Size(64, 13);
+            this.TLabelPTotal.TabIndex = 31;
+            this.TLabelPTotal.Text = "Precio Total";
+            // 
+            // TTextBoxCategoria
+            // 
+            this.TTextBoxCategoria.Location = new System.Drawing.Point(119, 143);
+            this.TTextBoxCategoria.Name = "TTextBoxCategoria";
+            this.TTextBoxCategoria.Size = new System.Drawing.Size(61, 20);
+            this.TTextBoxCategoria.TabIndex = 32;
+            // 
+            // TTextBoxVehiculo
+            // 
+            this.TTextBoxVehiculo.Location = new System.Drawing.Point(119, 175);
+            this.TTextBoxVehiculo.Name = "TTextBoxVehiculo";
+            this.TTextBoxVehiculo.Size = new System.Drawing.Size(61, 20);
+            this.TTextBoxVehiculo.TabIndex = 33;
+            // 
+            // TTextBoxConductores
+            // 
+            this.TTextBoxConductores.Location = new System.Drawing.Point(119, 205);
+            this.TTextBoxConductores.Name = "TTextBoxConductores";
+            this.TTextBoxConductores.Size = new System.Drawing.Size(60, 20);
+            this.TTextBoxConductores.TabIndex = 34;
+            // 
+            // TTextBoxTarifa
+            // 
+            this.TTextBoxTarifa.Location = new System.Drawing.Point(119, 237);
+            this.TTextBoxTarifa.Name = "TTextBoxTarifa";
+            this.TTextBoxTarifa.Size = new System.Drawing.Size(60, 20);
+            this.TTextBoxTarifa.TabIndex = 35;
+            // 
+            // TTextBoxTiempo
+            // 
+            this.TTextBoxTiempo.Location = new System.Drawing.Point(119, 267);
+            this.TTextBoxTiempo.Name = "TTextBoxTiempo";
+            this.TTextBoxTiempo.Size = new System.Drawing.Size(61, 20);
+            this.TTextBoxTiempo.TabIndex = 36;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(291, 287);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(51, 20);
+            this.textBox1.TabIndex = 37;
+            // 
+            // TTextBoxTPSinIva
+            // 
+            this.TTextBoxTPSinIva.Location = new System.Drawing.Point(35, 362);
+            this.TTextBoxTPSinIva.Name = "TTextBoxTPSinIva";
+            this.TTextBoxTPSinIva.Size = new System.Drawing.Size(76, 20);
+            this.TTextBoxTPSinIva.TabIndex = 38;
+            // 
+            // TTextBoxTIva
+            // 
+            this.TTextBoxTIva.Location = new System.Drawing.Point(167, 361);
+            this.TTextBoxTIva.Name = "TTextBoxTIva";
+            this.TTextBoxTIva.Size = new System.Drawing.Size(48, 20);
+            this.TTextBoxTIva.TabIndex = 39;
+            // 
+            // TTextBoxPTotal
+            // 
+            this.TTextBoxPTotal.Location = new System.Drawing.Point(289, 359);
+            this.TTextBoxPTotal.Name = "TTextBoxPTotal";
+            this.TTextBoxPTotal.Size = new System.Drawing.Size(61, 20);
+            this.TTextBoxPTotal.TabIndex = 40;
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rectangleShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(389, 445);
+            this.shapeContainer1.TabIndex = 41;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // rectangleShape1
+            // 
+            this.rectangleShape1.Location = new System.Drawing.Point(11, 93);
+            this.rectangleShape1.Name = "rectangleShape1";
+            this.rectangleShape1.Size = new System.Drawing.Size(362, 303);
+            // 
+            // TButtonDuplicar
+            // 
+            this.TButtonDuplicar.Location = new System.Drawing.Point(31, 405);
+            this.TButtonDuplicar.Name = "TButtonDuplicar";
+            this.TButtonDuplicar.Size = new System.Drawing.Size(76, 28);
+            this.TButtonDuplicar.TabIndex = 42;
+            this.TButtonDuplicar.Text = "Duplicar";
+            this.TButtonDuplicar.UseVisualStyleBackColor = true;
+            // 
+            // TButtonModificar
+            // 
+            this.TButtonModificar.Location = new System.Drawing.Point(150, 405);
+            this.TButtonModificar.Name = "TButtonModificar";
+            this.TButtonModificar.Size = new System.Drawing.Size(76, 28);
+            this.TButtonModificar.TabIndex = 43;
+            this.TButtonModificar.Text = "Modificar";
+            this.TButtonModificar.UseVisualStyleBackColor = true;
+            // 
+            // TButtonEliminar
+            // 
+            this.TButtonEliminar.Location = new System.Drawing.Point(275, 405);
+            this.TButtonEliminar.Name = "TButtonEliminar";
+            this.TButtonEliminar.Size = new System.Drawing.Size(76, 28);
+            this.TButtonEliminar.TabIndex = 44;
+            this.TButtonEliminar.Text = "Eliminar";
+            this.TButtonEliminar.UseVisualStyleBackColor = true;
+            // 
+            // GestionFacturacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 445);
+            this.Controls.Add(this.TButtonEliminar);
+            this.Controls.Add(this.TButtonModificar);
+            this.Controls.Add(this.TButtonDuplicar);
+            this.Controls.Add(this.TTextBoxPTotal);
+            this.Controls.Add(this.TTextBoxTIva);
+            this.Controls.Add(this.TTextBoxTPSinIva);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TTextBoxTiempo);
+            this.Controls.Add(this.TTextBoxTarifa);
+            this.Controls.Add(this.TTextBoxConductores);
+            this.Controls.Add(this.TTextBoxVehiculo);
+            this.Controls.Add(this.TTextBoxCategoria);
+            this.Controls.Add(this.TLabelPTotal);
+            this.Controls.Add(this.TLabelTIva);
+            this.Controls.Add(this.TLabelPSinIva);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TLabelTiempo);
+            this.Controls.Add(this.TLabelTarifa);
+            this.Controls.Add(this.TLabelConductores);
+            this.Controls.Add(this.TLabelVehiculo);
+            this.Controls.Add(this.TLabelCategoria);
+            this.Controls.Add(this.TLabelEmpresa);
+            this.Controls.Add(this.TLabelUsuario);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TButtonBuscar);
+            this.Controls.Add(this.TTextBoxNFactura);
+            this.Controls.Add(this.TLabelNFactura);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "GestionFacturacion";
+            this.Text = "Gestión Facturas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TLabelNFactura;
+        private System.Windows.Forms.TextBox TTextBoxNFactura;
+        private System.Windows.Forms.Button TButtonBuscar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label TLabelUsuario;
+        private System.Windows.Forms.Label TLabelEmpresa;
+        private System.Windows.Forms.Label TLabelCategoria;
+        private System.Windows.Forms.Label TLabelVehiculo;
+        private System.Windows.Forms.Label TLabelConductores;
+        private System.Windows.Forms.Label TLabelTarifa;
+        private System.Windows.Forms.Label TLabelTiempo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label TLabelPSinIva;
+        private System.Windows.Forms.Label TLabelTIva;
+        private System.Windows.Forms.Label TLabelPTotal;
+        private System.Windows.Forms.TextBox TTextBoxCategoria;
+        private System.Windows.Forms.TextBox TTextBoxVehiculo;
+        private System.Windows.Forms.TextBox TTextBoxConductores;
+        private System.Windows.Forms.TextBox TTextBoxTarifa;
+        private System.Windows.Forms.TextBox TTextBoxTiempo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TTextBoxTPSinIva;
+        private System.Windows.Forms.TextBox TTextBoxTIva;
+        private System.Windows.Forms.TextBox TTextBoxPTotal;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape1;
+        private System.Windows.Forms.Button TButtonDuplicar;
+        private System.Windows.Forms.Button TButtonModificar;
+        private System.Windows.Forms.Button TButtonEliminar;
+    }
+}
