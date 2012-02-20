5349fd6bedcd47a86036e490e2497bced6bc2b3b
dsarabia.m@gmail.com <dsarabia.m@gmail.com@99eea89f-4a7c-9688-262d-877626069f20>
2012-02-20 13:27:30 +0000

    nueva pantalla de gestion de personal

---
+++ trunk/AlquilerCoches/AlquilerCoches/GestionPersonal.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace AlquilerCoches
+{
+    partial class GestionPersonal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // GestionPersonal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 303);
+            this.Name = "GestionPersonal";
+            this.Text = "GestionPersonal";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
