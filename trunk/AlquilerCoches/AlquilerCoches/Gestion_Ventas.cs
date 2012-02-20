34c2a9ddc9507c2c2e1504033c3542fd81f400a8
javiumbe@gmail.com <javiumbe@gmail.com@99eea89f-4a7c-9688-262d-877626069f20>
2012-02-20 13:22:27 +0000

    Añadida la primera pantalla de gestión de ventas. Corresponde con la opción "Poner en Venta".

---
+++ trunk/AlquilerCoches/AlquilerCoches/Gestion_Ventas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AlquilerCoches
+{
+    public partial class Gestion_Ventas : Form
+    {
+        public Gestion_Ventas()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Gestion_Ventas_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
