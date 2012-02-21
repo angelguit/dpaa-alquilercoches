1a485cd6c24fe0aad11864649657ff9d9afebbb8
s2ph3r4t <s2ph3r4t@99eea89f-4a7c-9688-262d-877626069f20>
2012-02-21 08:26:48 +0000

    Pantalla Gestion Proveedores

---
+++ trunk/AlquilerCoches/AlquilerCoches/GestionProveedores.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AlquilerCoches
+{
+    public partial class GestionProveedores : Form
+    {
+        public GestionProveedores()
+        {
+            InitializeComponent();
+        }
+    }
+}
