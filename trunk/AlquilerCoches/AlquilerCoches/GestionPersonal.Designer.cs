8c95e31064c119e80540b2163c184114dcdd5826
dsarabia.m@gmail.com <dsarabia.m@gmail.com@99eea89f-4a7c-9688-262d-877626069f20>
2012-02-22 14:56:54 +0000

    elimninado salvapantallas

--- trunk/AlquilerCoches/AlquilerCoches/GestionPersonal.Designer.cs
+++ trunk/AlquilerCoches/AlquilerCoches/GestionPersonal.Designer.cs
@@ -243,7 +243,6 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::AlquilerCoches.Properties.Resources.rentcar;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(610, 368);
             this.Controls.Add(this.TButtonFoto);
