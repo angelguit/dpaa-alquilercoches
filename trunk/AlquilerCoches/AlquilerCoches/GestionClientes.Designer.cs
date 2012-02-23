659b6531bfbdfc533583aa0e2a85e463407fd16f
dsarabia.m@gmail.com <dsarabia.m@gmail.com@99eea89f-4a7c-9688-262d-877626069f20>
2012-02-23 13:15:41 +0000

    gestion clientes

--- trunk/AlquilerCoches/AlquilerCoches/GestionClientes.Designer.cs
+++ trunk/AlquilerCoches/AlquilerCoches/GestionClientes.Designer.cs
@@ -28,11 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TLabelTelefono2 = new System.Windows.Forms.Label();
+            this.TTextBoxProvincia = new System.Windows.Forms.TextBox();
+            this.TTextBoxCiudad = new System.Windows.Forms.TextBox();
+            this.TTextBoxTelefono = new System.Windows.Forms.TextBox();
+            this.TTextBoxApellidos = new System.Windows.Forms.TextBox();
+            this.TTextBoxNombre = new System.Windows.Forms.TextBox();
+            this.TTextBoxDNI = new System.Windows.Forms.TextBox();
+            this.TLabelProvincia = new System.Windows.Forms.Label();
+            this.TLabelCiudad = new System.Windows.Forms.Label();
+            this.TLabelTelefono1 = new System.Windows.Forms.Label();
+            this.TLabelApellidos = new System.Windows.Forms.Label();
+            this.TLabelNombre = new System.Windows.Forms.Label();
+            this.TLabelDNI = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TLabelDireccion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(108, 20);
+            this.textBox1.TabIndex = 36;
+            // 
+            // TLabelTelefono2
+            // 
+            this.TLabelTelefono2.AutoSize = true;
+            this.TLabelTelefono2.Location = new System.Drawing.Point(16, 122);
+            this.TLabelTelefono2.Name = "TLabelTelefono2";
+            this.TLabelTelefono2.Size = new System.Drawing.Size(58, 13);
+            this.TLabelTelefono2.TabIndex = 35;
+            this.TLabelTelefono2.Text = "Telefono 2";
+            // 
+            // TTextBoxProvincia
+            // 
+            this.TTextBoxProvincia.Location = new System.Drawing.Point(92, 183);
+            this.TTextBoxProvincia.Name = "TTextBoxProvincia";
+            this.TTextBoxProvincia.Size = new System.Drawing.Size(108, 20);
+            this.TTextBoxProvincia.TabIndex = 33;
+            // 
+            // TTextBoxCiudad
+            // 
+            this.TTextBoxCiudad.Location = new System.Drawing.Point(92, 161);
+            this.TTextBoxCiudad.Name = "TTextBoxCiudad";
+            this.TTextBoxCiudad.Size = new System.Drawing.Size(108, 20);
+            this.TTextBoxCiudad.TabIndex = 32;
+            // 
+            // TTextBoxTelefono
+            // 
+            this.TTextBoxTelefono.Location = new System.Drawing.Point(92, 92);
+            this.TTextBoxTelefono.Name = "TTextBoxTelefono";
+            this.TTextBoxTelefono.Size = new System.Drawing.Size(108, 20);
+            this.TTextBoxTelefono.TabIndex = 31;
+            // 
+            // TTextBoxApellidos
+            // 
+            this.TTextBoxApellidos.Location = new System.Drawing.Point(92, 70);
+            this.TTextBoxApellidos.Name = "TTextBoxApellidos";
+            this.TTextBoxApellidos.Size = new System.Drawing.Size(108, 20);
+            this.TTextBoxApellidos.TabIndex = 30;
+            // 
+            // TTextBoxNombre
+            // 
+            this.TTextBoxNombre.Location = new System.Drawing.Point(92, 48);
+            this.TTextBoxNombre.Name = "TTextBoxNombre";
+            this.TTextBoxNombre.Size = new System.Drawing.Size(70, 20);
+            this.TTextBoxNombre.TabIndex = 29;
+            // 
+            // TTextBoxDNI
+            // 
+            this.TTextBoxDNI.Location = new System.Drawing.Point(92, 25);
+            this.TTextBoxDNI.Name = "TTextBoxDNI";
+            this.TTextBoxDNI.Size = new System.Drawing.Size(70, 20);
+            this.TTextBoxDNI.TabIndex = 28;
+            // 
+            // TLabelProvincia
+            // 
+            this.TLabelProvincia.AutoSize = true;
+            this.TLabelProvincia.Location = new System.Drawing.Point(25, 190);
+            this.TLabelProvincia.Name = "TLabelProvincia";
+            this.TLabelProvincia.Size = new System.Drawing.Size(51, 13);
+            this.TLabelProvincia.TabIndex = 27;
+            this.TLabelProvincia.Text = "Provincia";
+            // 
+            // TLabelCiudad
+            // 
+            this.TLabelCiudad.AutoSize = true;
+            this.TLabelCiudad.Location = new System.Drawing.Point(34, 168);
+            this.TLabelCiudad.Name = "TLabelCiudad";
+            this.TLabelCiudad.Size = new System.Drawing.Size(40, 13);
+            this.TLabelCiudad.TabIndex = 26;
+            this.TLabelCiudad.Text = "Ciudad";
+            // 
+            // TLabelTelefono1
+            // 
+            this.TLabelTelefono1.AutoSize = true;
+            this.TLabelTelefono1.Location = new System.Drawing.Point(16, 99);
+            this.TLabelTelefono1.Name = "TLabelTelefono1";
+            this.TLabelTelefono1.Size = new System.Drawing.Size(58, 13);
+            this.TLabelTelefono1.TabIndex = 25;
+            this.TLabelTelefono1.Text = "Telefono 1";
+            // 
+            // TLabelApellidos
+            // 
+            this.TLabelApellidos.AutoSize = true;
+            this.TLabelApellidos.Location = new System.Drawing.Point(25, 77);
+            this.TLabelApellidos.Name = "TLabelApellidos";
+            this.TLabelApellidos.Size = new System.Drawing.Size(49, 13);
+            this.TLabelApellidos.TabIndex = 24;
+            this.TLabelApellidos.Text = "Apellidos";
+            // 
+            // TLabelNombre
+            // 
+            this.TLabelNombre.AutoSize = true;
+            this.TLabelNombre.Location = new System.Drawing.Point(30, 55);
+            this.TLabelNombre.Name = "TLabelNombre";
+            this.TLabelNombre.Size = new System.Drawing.Size(44, 13);
+            this.TLabelNombre.TabIndex = 23;
+            this.TLabelNombre.Text = "Nombre";
+            // 
+            // TLabelDNI
+            // 
+            this.TLabelDNI.AutoSize = true;
+            this.TLabelDNI.Location = new System.Drawing.Point(48, 32);
+            this.TLabelDNI.Name = "TLabelDNI";
+            this.TLabelDNI.Size = new System.Drawing.Size(26, 13);
+            this.TLabelDNI.TabIndex = 22;
+            this.TLabelDNI.Text = "DNI";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(92, 137);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(108, 20);
+            this.textBox2.TabIndex = 38;
+            // 
+            // TLabelDireccion
+            // 
+            this.TLabelDireccion.AutoSize = true;
+            this.TLabelDireccion.Location = new System.Drawing.Point(22, 144);
+            this.TLabelDireccion.Name = "TLabelDireccion";
+            this.TLabelDireccion.Size = new System.Drawing.Size(52, 13);
+            this.TLabelDireccion.TabIndex = 37;
+            this.TLabelDireccion.Text = "Direccion";
+            this.TLabelDireccion.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // GestionClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 370);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.TLabelDireccion);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TLabelTelefono2);
+            this.Controls.Add(this.TTextBoxProvincia);
+            this.Controls.Add(this.TTextBoxCiudad);
+            this.Controls.Add(this.TTextBoxTelefono);
+            this.Controls.Add(this.TTextBoxApellidos);
+            this.Controls.Add(this.TTextBoxNombre);
+            this.Controls.Add(this.TTextBoxDNI);
+            this.Controls.Add(this.TLabelProvincia);
+            this.Controls.Add(this.TLabelCiudad);
+            this.Controls.Add(this.TLabelTelefono1);
+            this.Controls.Add(this.TLabelApellidos);
+            this.Controls.Add(this.TLabelNombre);
+            this.Controls.Add(this.TLabelDNI);
+            this.Name = "GestionClientes";
             this.Text = "GestionClientes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label TLabelTelefono2;
+        private System.Windows.Forms.TextBox TTextBoxProvincia;
+        private System.Windows.Forms.TextBox TTextBoxCiudad;
+        private System.Windows.Forms.TextBox TTextBoxTelefono;
+        private System.Windows.Forms.TextBox TTextBoxApellidos;
+        private System.Windows.Forms.TextBox TTextBoxNombre;
+        private System.Windows.Forms.TextBox TTextBoxDNI;
+        private System.Windows.Forms.Label TLabelProvincia;
+        private System.Windows.Forms.Label TLabelCiudad;
+        private System.Windows.Forms.Label TLabelTelefono1;
+        private System.Windows.Forms.Label TLabelApellidos;
+        private System.Windows.Forms.Label TLabelNombre;
+        private System.Windows.Forms.Label TLabelDNI;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label TLabelDireccion;
+
     }
 }
