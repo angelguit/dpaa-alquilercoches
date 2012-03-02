924f6bc821b49938086be0bc6e457f302fe65b2d
dsarabia.m@gmail.com <dsarabia.m@gmail.com@99eea89f-4a7c-9688-262d-877626069f20>
2012-03-02 00:45:14 +0000

    añadida nuesvas caracteristicas a la tabla de gestion de personal, añadido boton editar y opcion para eliminar

--- trunk/AlquilerCoches/AlquilerCoches/GestionPersonalBuscar.cs
+++ trunk/AlquilerCoches/AlquilerCoches/GestionPersonalBuscar.cs
@@ -14,6 +14,37 @@ namespace AlquilerCoches
         public GestionPersonalBuscar()
         {
             InitializeComponent();
+
+            /*  DataGridViewButtonColumn colBotones = new DataGridViewButtonColumn();
+              colBotones.Name = "colBotones";
+              colBotones.HeaderText = "Editar";
+              colBotones.ReadOnly = true;
+              colBotones.Text="Editar";
+              colBotones.
+
+              this.TDataGridViewPersonal.Columns.Add(colBotones);*/
+            DataGridViewButtonColumn buttons = new DataGridViewButtonColumn();
+            {
+                buttons.HeaderText = "Editar"; //texto de la columna
+                buttons.Text = "Editar"; //texto de cada boton, sale al introducir texto
+                buttons.UseColumnTextForButtonValue = true;
+                buttons.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                buttons.FlatStyle = FlatStyle.Standard;
+                buttons.CellTemplate.Style.BackColor = Color.Honeydew;
+                buttons.DisplayIndex = 0;
+            }
+
+            DataGridViewCheckBoxColumn boton = new DataGridViewCheckBoxColumn();
+            {
+                boton.HeaderText = "Eliminar";//texto de la columna
+                boton.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells; //tamaño
+                boton.DisplayIndex = 0; //indice que ocupara en la tabla
+
+            }
+
+
+            TDataGridViewPersonal.Columns.Add(boton);
+            TDataGridViewPersonal.Columns.Add(buttons);
         }
 
         private void TButtonBuscar_Click(object sender, EventArgs e)
@@ -21,5 +52,7 @@ namespace AlquilerCoches
             //if(hay resultados) mostrar boton eliminar
             TButtonEliminar.Visible = true;
         }
+
+
     }
 }
