280cfde8a6e9c342b7b2113487b5f03754d4b5d8
dsarabia.m@gmail.com <dsarabia.m@gmail.com@99eea89f-4a7c-9688-262d-877626069f20>
2012-03-06 11:47:59 +0000

    cambios expresiones regulares gestion personal

--- trunk/AlquilerCoches/AlquilerCoches/GestionPersonal.cs
+++ trunk/AlquilerCoches/AlquilerCoches/GestionPersonal.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace AlquilerCoches
 {
@@ -31,9 +32,110 @@ namespace AlquilerCoches
 
         }
 
-        private void GestionPersonal_Load(object sender, EventArgs e)
+        bool incorrecto = true;
+        private void TTextBoxDNI_Leave(object sender, EventArgs e)
         {
-
+            if (!Regex.Match(TTextBoxDNI.Text, @"^(([A-Z]\d{8})|(\d{8}[A-Z]))$").Success)
+            {
+                errorProvider1.SetError(TTextBoxDNI, "DNI incorrecto");
+                incorrecto = true;
+            }
+            else { incorrecto = false; errorProvider1.SetError(TTextBoxDNI, ""); }
         }
+        private void TTextBoxNombre_Leave(object sender, EventArgs e)
+        {
+            if (!Regex.Match(TTextBoxNombre.Text, @"^[A-Za-z]{3,15}$").Success)
+            {
+                errorProvider1.SetError(TTextBoxNombre, "Nombre incorrecto");
+                incorrecto = true;
+            }
+            else { incorrecto = false; errorProvider1.SetError(TTextBoxNombre, ""); }
+        }
+        private void TTextBoxApellidos_Leave(object sender, EventArgs e)
+        {
+            if (!Regex.Match(TTextBoxApellidos.Text, @"^[A-Za-z]{3,40}$").Success)
+            {
+                errorProvider1.SetError(TTextBoxApellidos, "Apellidos incorrectos, caracteres invalidos");
+                incorrecto = true;
+            }
+            else { incorrecto = false; errorProvider1.SetError(TTextBoxApellidos, ""); }
+        }
+
+        private void TTextBoxTelefono_Leave(object sender, EventArgs e)
+        {
+            if (!Regex.Match(TTextBoxTelefono.Text, @"^[0-9]{9}$").Success)
+            {
+                errorProvider1.SetError(TTextBoxTelefono, "Telefono incorrecto, caracteres invalidos");
+                incorrecto = true;
+            }
+            else { incorrecto = false; errorProvider1.SetError(TTextBoxTelefono, ""); }
+        }
+
+        private void TTextBoxEmail_Leave(object sender, EventArgs e)
+        {
+            if (!Regex.Match(TTextBoxEmail.Text, @"/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/").Success)
+            {
+                errorProvider1.SetError(TTextBoxEmail, "Email incorrectos, caracteres invalidos");
+                incorrecto = true;
+            }
+            else { incorrecto = false; errorProvider1.SetError(TTextBoxEmail, ""); }
+        }
+
+        private void TTextBoxDireccion_Leave(object sender, EventArgs e)
+        {
+            if (!Regex.Match(TTextBoxDireccion.Text, @"^[A-Za-z]{3,40}$").Success)
+            {
+                errorProvider1.SetError(TTextBoxDireccion, "Dirección incorrecta, caracteres invalidos");
+                incorrecto = true;
+            }
+            else { incorrecto = false; errorProvider1.SetError(TTextBoxDireccion, ""); }
+        }
+
+        private void TTextBoxCiudad_Leave(object sender, EventArgs e)
+        {
+            if (!Regex.Match(TTextBoxCiudad.Text, @"^[A-Za-z]{3,40}$").Success)
+            {
+                errorProvider1.SetError(TTextBoxCiudad, "Ciudad incorrecta, caracteres invalidos");
+                incorrecto = true;
+            }
+            else { incorrecto = false; errorProvider1.SetError(TTextBoxCiudad, ""); }
+        }
+
+        private void TTextBoxProvincia_Leave(object sender, EventArgs e)
+        {
+            if (!Regex.Match(TTextBoxProvincia.Text, @"^[A-Za-z]{3,40}$").Success)
+            {
+                errorProvider1.SetError(TTextBoxProvincia, "Provincia incorrecta, caracteres invalidos");
+                incorrecto = true;
+            }
+            else { incorrecto = false; errorProvider1.SetError(TTextBoxProvincia, ""); }
+        }
+
+        private void TTextBoxPuestoAc_Leave(object sender, EventArgs e)
+        {
+            if (!Regex.Match(TTextBoxPuestoAc.Text, @"^[A-Za-z]{3,15}$").Success)
+            {
+                errorProvider1.SetError(TTextBoxPuestoAc, "Puesto incorrecto, caracteres invalidos");
+                incorrecto = true;
+            }
+            else { incorrecto = false; errorProvider1.SetError(TTextBoxPuestoAc, ""); }
+        }
+
+        private void TButtonGuardarPersonal_Click(object sender, EventArgs e)
+        {
+            if (incorrecto == true)
+            {
+                MessageBox.Show("Campos invalidos, reviselos", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+           
+        }
+
+
+
+
+        
+
+
     }
 }
