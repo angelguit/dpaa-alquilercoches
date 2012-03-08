81042a1ca5a5fcb335c3440d1fab09b3a9536bc4
jorgedgm11 <jorgedgm11@99eea89f-4a7c-9688-262d-877626069f20>
2012-03-08 12:26:58 +0000

    gestion proveedores

--- trunk/AlquilerCoches/AlquilerCoches/Program.cs
+++ trunk/AlquilerCoches/AlquilerCoches/Program.cs
@@ -15,7 +15,7 @@ namespace AlquilerCoches
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new GestionProveedores());
         }
     }
 }
