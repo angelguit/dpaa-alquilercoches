edbdb811ff1f666ea754a24e16990fc826f7da0e
s2ph3r4t <s2ph3r4t@99eea89f-4a7c-9688-262d-877626069f20>
2012-03-09 08:33:43 +0000

    Agrados modelos de EN y CAD

---
+++ AlquilerCoches/AlquilerCoches/CAD/CADVehiculo.cs
@@ -0,0 +1,39 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Collections;
+
+namespace AlquilerCoches.CAD
+{
+    public class CADVehiculo
+    {
+        static private String cadenaConexion = "Data Source=|DataDirectory|\BBDD.sdf";
+        static private String nombreTabla = "Vehiculo";
+
+        public DataSet ObtenerTablaVehiculo()
+        {
+            DataSet dsVehiculo = new DataSet();
+            
+            try
+            {
+                SqlConnection conexion = new SqlConnection(cadenaConexion);
+                String consulta = "Select * from Vehiculo";
+                SqlDataAdapter daCaminos = new SqlDataAdapter(consulta, conexion);
+                daCaminos.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+                daCaminos.Fill(dsVehiculo, nombreTabla);
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+
+            return dsVehiculo;
+        }
+    }
+}
--- AlquilerCoches/AlquilerCoches/EN/ENVehiculo.cs
+++ AlquilerCoches/AlquilerCoches/EN/ENVehiculo.cs
@@ -2,11 +2,97 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Collections;
 
 namespace AlquilerCoches.EN
 {
-    class ENVehiculo
+    public class ENVehiculo
     {
+        private string matricula;
+        private string marca;
+        private string modelo;
+        private int anyo;
+        private int precioVenta;
+        private int precioCompra;
+        private int precioAlquiler;
+        private int km;
+        private int estado;
+        private int garantia;
 
+        private CAD.CADVehiculo cadVehiculo = new CAD.CADVehiculo();
+
+        public DataSet ObtenerListaVehiculos()
+        {
+            DataSet resultado = new DataSet();
+            try
+            {
+                resultado = cadVehiculo.ObtenerTablaVehiculo();
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+
+            return resultado;
+        }
+
+
+
+
+        public string Matricula
+        {
+            get { return matricula; }
+            set { matricula = Matricula; }
+        }
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = Marca; }
+        }
+        public string Modelo
+        {
+            get { return modelo; }
+            set { modelo = Modelo; }
+        }
+        public int Anyo
+        {
+            get { return anyo; }
+            set { anyo = Anyo; }
+        }
+        public int PrecioVenta
+        {
+            get { return precioVenta; }
+            set { precioVenta = PrecioVenta; }
+        }
+        public int PrecioCompra
+        {
+            get { return precioCompra; }
+            set { precioCompra = PrecioCompra; }
+        }
+        public int PrecioAlquiler
+        {
+            get { return precioAlquiler; }
+            set { precioAlquiler = PrecioAlquiler; }
+        }
+        public int KM
+        {
+            get { return km; }
+            set { km = KM; }
+        }
+        public int Estado
+        {
+            get { return estado; }
+            set { estado = Estado; }
+        }
+        public int Garantia
+        {
+            get { return garantia; }
+            set { garantia = Garantia; }
+        }
     }
 }
