77fe3764c3270ca38e75ed026b0e21dcd7893620
javiumbe <javiumbe@99eea89f-4a7c-9688-262d-877626069f20>
2012-03-12 13:53:34 +0000

    Añadido CADVentas

---
+++ trunk/AlquilerCoches/AlquilerCoches/CAD/CADVentas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AlquilerCoches.CAD
+{
+    public class CADVehiculo
+    {
+        static private String cadenaConexion = @"Data Source=|DataDirectory|\BBDD.sdf";
+        static private String nombreTabla = "Vehiculo";
+
+        public DataSet ObtenerTablaVentas()
+        {
+            DataSet deVentas = new DataSet();
+
+            try
+            {
+                SqlConnection conexion = new SqlConnection(cadenaConexion);
+                String consulta = "Select * from Ventas";
+                SqlDataAdapter daCaminos = new SqlDataAdapter(consulta, conexion);
+                daCaminos.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+                daCaminos.Fill(deVentas, nombreTabla);
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+
+            return deVentas;
+        }
+    }
+}
