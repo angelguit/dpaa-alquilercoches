0a3db5c4ad8f32fca08136457ecd8f9907a219b1
javiumbe <javiumbe@99eea89f-4a7c-9688-262d-877626069f20>
2012-03-12 13:03:31 +0000

    Añadido ENVentas

---
+++ trunk/AlquilerCoches/AlquilerCoches/EN/ENVentas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlquilerCoches.EN
+{
+    public class ENVentas
+    {
+        private DateTime fechaVenta;
+        private string numVend;
+        private string numCliente;
+        private string numFactura;
+        private int precioVenta;
+
+        public DateTime FechaVenta
+        {
+            get { return fechaVenta; }
+            set { fechaVenta = FechaVenta; }
+        }
+
+        public string NumVend
+        {
+            get { return numVend; }
+            set { numVend = NumVend; }
+        }
+
+        public string NumCliente
+        {
+            get { return numCliente; }
+            set { numCliente = NumCliente; }
+        }
+
+        public string NumFactura
+        {
+            get { return numFactura; }
+            set { numFactura = NumFactura; }
+        }
+
+        public int PrecioVenta
+        {
+            get { return precioVenta; }
+            set { precioVenta = PrecioVenta; }
+        }
+    }
+}
