04a7510c829f119e12687a30dca124ff5481db04
s2ph3r4t <s2ph3r4t@99eea89f-4a7c-9688-262d-877626069f20>
2012-03-15 12:24:07 +0000

    BBDD

--- trunk/AlquilerCoches/AlquilerCoches/Properties/Settings.Designer.cs
+++ trunk/AlquilerCoches/AlquilerCoches/Properties/Settings.Designer.cs
@@ -32,5 +32,16 @@ namespace AlquilerCoches.Properties {
                 return ((string)(this["BBDDConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;AttachDbFilename=|DataDirectory|\\BBDD.mdf;Integrated Sec" +
+            "urity=True;User Instance=True")]
+        public string BBDDConnectionString1 {
+            get {
+                return ((string)(this["BBDDConnectionString1"]));
+            }
+        }
     }
 }
