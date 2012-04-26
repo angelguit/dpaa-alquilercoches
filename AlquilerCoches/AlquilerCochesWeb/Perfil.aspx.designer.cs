dd646fe7d615ae3b5d68244b8e671ea0fb1b0f8a
dsarabia.m <dsarabia.m@gmail.com>
2012-04-26 16:14:15 +0000

    cambios en Perfil web

--- AlquilerCoches/AlquilerCochesWeb/Perfil.aspx.designer.cs
+++ AlquilerCoches/AlquilerCochesWeb/Perfil.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace AlquilerCochesWeb {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control TMenuPerfil.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu TMenuPerfil;
     }
 }
