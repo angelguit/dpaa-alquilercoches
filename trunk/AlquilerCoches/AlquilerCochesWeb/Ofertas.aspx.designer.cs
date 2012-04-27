d66a47ddbb5f1550b451ace33ed7bbbfebbc84e1
s2ph3r4t <s2ph3r4t@99eea89f-4a7c-9688-262d-877626069f20>
2012-04-27 17:39:31 +0000

    Spike2

--- trunk/AlquilerCoches/AlquilerCochesWeb/Ofertas.aspx.designer.cs
+++ trunk/AlquilerCoches/AlquilerCochesWeb/Ofertas.aspx.designer.cs
@@ -40,6 +40,24 @@ namespace AlquilerCochesWeb {
         protected global::System.Web.UI.WebControls.View View1;
         
         /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Control Image2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image2;
+        
+        /// <summary>
         /// Control View2.
         /// </summary>
         /// <remarks>
