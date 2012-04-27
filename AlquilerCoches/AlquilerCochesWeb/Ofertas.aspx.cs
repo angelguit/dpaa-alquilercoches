558714a854983f1d10a68816ee3108aa285ec19b
s2ph3r4t <s2ph3r4t@99eea89f-4a7c-9688-262d-877626069f20>
2012-04-27 10:53:17 +0000

    Spike: Prueba de controles y estilos.

--- AlquilerCoches/AlquilerCochesWeb/Ofertas.aspx.cs
+++ AlquilerCoches/AlquilerCochesWeb/Ofertas.aspx.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
 
 namespace AlquilerCochesWeb
 {
@@ -13,5 +18,11 @@ namespace AlquilerCochesWeb
         {
 
         }
+
+        protected void Menu1_MenuItemClick(object sender, MenuEventArgs e)
+        {
+            
+        }
+
     }
 }
